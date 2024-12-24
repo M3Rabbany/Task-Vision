6d2ea8b3588dd3b593135f3f6e4849bec0445460
Hilman <hichanhilman6@gmail.com>
2024-12-24 14:57:48 +0700

    feat: add validation

---
+++ src/main/java/com/red/team/taskvisionapp/service/impl/ValidationServiceImpl.java
@@ -0,0 +1,26 @@
+package com.red.team.taskvisionapp.service.impl;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+import jakarta.validation.Validator;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+@RequiredArgsConstructor
+public class ValidationServiceImpl {
+    private final Validator validator;
+
+    public void validate(Object request){
+        Set<ConstraintViolation<Object>> constraintViolations = validator.validate(request);
+
+        if(!constraintViolations.isEmpty()){
+            throw new ConstraintViolationException(constraintViolations);
+        }
+    }
+}
+
+
+
