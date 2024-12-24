1dbf4a6d94f056826ec6c100ebd8d4d306175842
ibnu <mhmdibnurbbn01@gmail.com>
2024-12-24 20:15:01 +0700

    feat: Add endpoint getUserByEmail in Controller

--- src/main/java/com/red/team/taskvisionapp/controller/UserController.java
+++ src/main/java/com/red/team/taskvisionapp/controller/UserController.java
@@ -2,7 +2,9 @@ package com.red.team.taskvisionapp.controller;
 
 import com.red.team.taskvisionapp.model.dto.request.UpdateUserRequest;
 import com.red.team.taskvisionapp.model.dto.request.UserRequest;
+import com.red.team.taskvisionapp.model.dto.response.CommonResponse;
 import com.red.team.taskvisionapp.model.dto.response.UserResponse;
+import com.red.team.taskvisionapp.model.entity.UserAccount;
 import com.red.team.taskvisionapp.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -25,24 +27,48 @@ public class UserController {
         return ResponseEntity.ok(userService.getAllUsers());
     }
 
+    @GetMapping("/email/{email}")
+    public ResponseEntity<CommonResponse<UserAccount>> getUserByEmail(@PathVariable String email) {
+        UserAccount user = userService.getUserByEmail(email);
+        return ResponseEntity.ok(CommonResponse.<UserAccount>builder()
+                        .message("User found!")
+                        .data(user)
+                        .statusCode(HttpStatus.OK.value())
+                .build());
+    }
+
     @PostMapping
     @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<UserResponse> createUser(@RequestBody @Valid UserRequest request) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(userService.createUser(request));
+    public ResponseEntity<CommonResponse<UserResponse>> createUser(@RequestBody @Valid UserRequest request) {
+        UserResponse user = userService.createUser(request);
+        return ResponseEntity.ok(CommonResponse.<UserResponse>builder()
+                .message("User created successfully!")
+                .data(user)
+                .statusCode(HttpStatus.CREATED.value())
+                .build());
     }
 
     @PutMapping("/{id}")
     @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<UserResponse> updateUser(@PathVariable String id,
-                                                   @RequestBody @Valid UpdateUserRequest request) {
-        return ResponseEntity.ok(userService.updateUser(id, request));
+    public ResponseEntity<CommonResponse<UserResponse>> updateUser(
+            @PathVariable String id,
+            @RequestBody @Valid UpdateUserRequest request) {
+        UserResponse user = userService.updateUser(id, request);
+        return ResponseEntity.ok(CommonResponse.<UserResponse>builder()
+                .message("User updated successfully!")
+                .data(user)
+                .statusCode(HttpStatus.OK.value())
+                .build());
     }
 
     @DeleteMapping("/{id}")
     @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<Void> deleteUser(@PathVariable String id) {
+    public ResponseEntity<CommonResponse<Void>> deleteUser(@PathVariable String id) {
         userService.deleteUser(id);
-        return ResponseEntity.noContent().build();
+        return ResponseEntity.ok(CommonResponse.<Void>builder()
+                .message("User deleted successfully!")
+                .statusCode(HttpStatus.OK.value())
+                .build());
     }
 
 }
