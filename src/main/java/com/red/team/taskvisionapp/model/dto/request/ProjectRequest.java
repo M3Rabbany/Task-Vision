2304d484952012366126d895119ec46868a8eaae
bismanandasatria <bismanandasatria@gmail.com>
2024-12-26 19:42:12 +0700

    ADD: CRUD for Project

---
+++ src/main/java/com/red/team/taskvisionapp/model/dto/request/ProjectRequest.java
@@ -0,0 +1,22 @@
+package com.red.team.taskvisionapp.model.dto.request;
+
+import com.red.team.taskvisionapp.constant.ProjectStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ProjectRequest {
+    private String projectName;
+    private String description;
+    private ProjectStatus status;
+    private LocalDateTime deadline;
+    private List<String> userIds;
+}
