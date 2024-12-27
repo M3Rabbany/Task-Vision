8294da47e09db7049e0c4d59e69aab4aadc88fd2
ibnu <mhmdibnurbbn01@gmail.com>
2024-12-27 14:20:37 +0700

    feat: add request approve,assign,and feedback

---
+++ src/main/java/com/red/team/taskvisionapp/model/dto/request/TaskAssignRequest.java
@@ -0,0 +1,27 @@
+package com.red.team.taskvisionapp.model.dto.request;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class TaskAssignRequest {
+    @NotBlank(message = "Project Id is required!")
+    @JsonProperty("project_id")
+    private String projectId;
+    @NotBlank(message = "Task Id is required!")
+    @JsonProperty("task_id")
+    private String taskId;
+    @NotNull(message = "User Id cannot be null!")
+    @Size(min = 1, message = "User Id cannot be empty!")
+    @JsonProperty("user_id")
+    private String userId;
+}
